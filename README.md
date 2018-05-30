821c9b965e1968b550b74b3a7287020934bcf530
Marlena Kropidlowska-Okraska <m.k.kropidlowska@gmail.com>
2018-05-30 14:43:51 +0200

    Styles added.

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
-# Game of Life
+# Description
 
-- dodac wybor dla uzytkownika, jaka wielkosc planszy
-- dodac wzorki
+This simple version of famous "Game of life" was created to  present author's skill and her learning progress.
+
+Game was developed with pure JavaScript and styled with CSS.
 
 
-rules of game
+#Info about "Game of life"
 
-info abou game
+The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
 
-how is it built
+The game is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. One interacts with the Game of Life by creating an initial configuration and observing how it evolves, or, for advanced players, by creating patterns with particular properties. [source: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life]
 
-how to run
+#Rules
+The game is designed around the following simple rules:
+1) Any live cell with fewer than two live neighbors dies, as if caused by underpopulation.
+2) Any live cell with more than three live neighbors dies, as if by overcrowding.
+3) Any live cell with two or three live neighbors lives on to the next generation.
+4) Any dead cell with exactly three live neighbors becomes a live cell.
+The operation of the game starts with an initial configuration on a two dimensional grid. This infinite square grid consists of cells with two possible states, alive or dead. Each cell has eight neighbors, namely the eight cells that touch it. The game operates in iterations, called ticks. Each tick applies the four rules of the game to every cell on the board simultaneously. [source: http://web.stanford.edu/~cdebs/GameOfLife/]
 
+#Instalation and guidelines
+To install the project simply download it and run the index.html in your web browser.
+
+#How to play
+Create your pattern by simply clicking on cells. Click play button and see how cell society lives and dies.
--- app.js
+++ app.js
@@ -7,43 +7,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
         this.createBoard = function () {
-            var board = document.getElementById("board");
+            const board = document.getElementById("board");
 
-            board.style.width = boardWidth * 10 + 'px';
-            board.style.height = boardHeight * 10 + 'px';
+            board.style.width = boardWidth * 18 + 'px';
+            board.style.height = boardHeight * 18 + 'px';
 
-            var allCells = this.width * this.height;
+            const allCells = this.width * this.height;
 
-            for (var i = 0; i < allCells; i++) {
-                var newdiv = document.createElement("div");
+            for (let i = 0; i < allCells; i++) {
+                const newdiv = document.createElement("div");
                 board.appendChild(newdiv);
             }
-            this.cells = [];
+
             this.cells = document.querySelectorAll("#board > div");
 
-            for (var j = 0; j < this.cells.length; j++) {
-                this.cells[j].addEventListener("mouseover", function () {
+            for (let j = 0; j < this.cells.length; j++) {
+                this.cells[j].addEventListener("click", function () {
                     this.classList.toggle("live");
                 })
             }
         }
-
-
     }
 
     GameOfLife.prototype.cellIndex = function (x, y) {
-        var index = x + y * this.width;
+        const index = x + y * this.width;
         return (this.cells[index]);
     };
 
     GameOfLife.prototype.setCellState = function (x, y, state) {
-        var index = x + y * this.width;
-
-        if (state === "live") {
-            this.cells[index].classList.add("live");
-        } else {
-            this.cells[index].classList.remove("live");
-        }
+        const index = x + y * this.width;
+        this.cells[index].classList.toggle("live");
     };
 
 
@@ -54,50 +47,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
     GameOfLife.prototype.computeCellNextState = function (x, y) {
 
-        var neighbour1 = this.cells[x + (y - 1) * this.width];
-        var neighbour2 = this.cells[(x - 1) + (y - 1) * this.width];
-        var neighbour3 = this.cells[(x - 1) + y * this.width];
-        var neighbour4 = this.cells[(x - 1) + (y + 1) * this.width];
-        var neighbour5 = this.cells[x + (y + 1) * this.width];
-        var neighbour6 = this.cells[(x + 1) + (y + 1) * this.width];
-        var neighbour7 = this.cells[(x + 1) + y * this.width];
-        var neighbour8 = this.cells[(x + 1) + (y - 1) * this.width];
+        const neighbour1 = this.cells[x + (y - 1) * this.width];
+        const neighbour2 = this.cells[(x - 1) + (y - 1) * this.width];
+        const neighbour3 = this.cells[(x - 1) + y * this.width];
+        const neighbour4 = this.cells[(x - 1) + (y + 1) * this.width];
+        const neighbour5 = this.cells[x + (y + 1) * this.width];
+        const neighbour6 = this.cells[(x + 1) + (y + 1) * this.width];
+        const neighbour7 = this.cells[(x + 1) + y * this.width];
+        const neighbour8 = this.cells[(x + 1) + (y - 1) * this.width];
 
 
-        var cellCurrentState = [];
-        if (neighbour1 !== undefined && neighbour1.className === "live") {
+        const cellCurrentState = [];
+        if (neighbour1 !== undefined && neighbour1.classList.contains('live')) {
             cellCurrentState.push(neighbour1.className);
         }
 
-        if (neighbour2 !== undefined && neighbour2.className === "live") {
+        if (neighbour2 !== undefined && neighbour2.classList.contains('live')) {
             cellCurrentState.push(neighbour2.className);
         }
 
-        if (neighbour3 !== undefined && neighbour3.className === "live") {
+        if (neighbour3 !== undefined && neighbour3.classList.contains('live')) {
             cellCurrentState.push(neighbour3.className);
         }
 
-        if (neighbour4 !== undefined && neighbour4.className === "live") {
+        if (neighbour4 !== undefined && neighbour4.classList.contains('live')) {
             cellCurrentState.push(neighbour4.className);
         }
 
-        if (neighbour5 !== undefined && neighbour5.className === "live") {
+        if (neighbour5 !== undefined && neighbour5.classList.contains('live')) {
             cellCurrentState.push(neighbour5.className);
         }
 
-        if (neighbour6 !== undefined && neighbour6.className === "live") {
+        if (neighbour6 !== undefined && neighbour6.classList.contains('live')) {
             cellCurrentState.push(neighbour6.className);
         }
 
-        if (neighbour7 !== undefined && neighbour7.className === "live") {
+        if (neighbour7 !== undefined && neighbour7.classList.contains('live')) {
             cellCurrentState.push(neighbour7.className);
         }
 
-        if (neighbour8 !== undefined && neighbour8.className === "live") {
+        if (neighbour8 !== undefined && neighbour8.classList.contains('live')) {
             cellCurrentState.push(neighbour8.className);
         }
 
-        var numOfLivingCells = cellCurrentState.length;
+        const numOfLivingCells = cellCurrentState.length;
 
 
         if ((this.cells[x + y * this.width].className !== "live") && (numOfLivingCells === 3)) {
@@ -119,33 +112,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     GameOfLife.prototype.computeNextGeneration = function () {
-        /*this.newBoard = [];
-
-        for (var i = 0; i < this.width; i++) {
-            var x = i;
-            for (var j = 0; j < this.height; j++) {
-                var y = j;
-                this.newBoard.push(this.computeCellNextState(i, j));
-            }
-        }
-        return this.newBoard; */
-// Nie dzialalo, bo petle byly zle. Zewnetrzna petla musi byc po wysokosci, a wewnetrzna po szerokosci.
         this.newBoard = [];
-        for (var i = 0; i < this.height; i++) {
-            var y = i;
-            for (var j = 0; j < this.width; j++) {
-                var x = j;
+        for (let i = 0; i < this.height; i++) {
+            const y = i;
+            for (let j = 0; j < this.width; j++) {
+                const x = j;
                 this.newBoard.push(this.computeCellNextState(j, i));
             }
         }
         return this.newBoard;
-
     };
 
 
     GameOfLife.prototype.printNextGeneration = function () {
         this.computeNextGeneration();
-        for (var i = 0; i < this.cells.length; i++) {
+        for (let i = 0; i < this.cells.length; i++) {
             if (this.newBoard[i] === 1) {
                 this.cells[i].classList.add("live");
             } else if (this.newBoard[i] === 0) {
@@ -156,10 +137,10 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     //Dodaje eventy na buttonach, pozwalajace zaczac i zatrzymac gre.
-    var btnplay = document.getElementById("play");
-    var btnpause = document.getElementById("pause");
+    const btnplay = document.getElementById("play");
+    const btnpause = document.getElementById("pause");
 
-    var interval;
+    let interval = null;
     btnplay.addEventListener("click", function () {
         interval = setInterval(function () {
             game.printNextGeneration()
@@ -171,7 +152,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-    var game = new GameOfLife(50, 50);
+    const game = new GameOfLife(30, 30);
 
     game.createBoard();
 
